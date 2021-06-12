0567e963819c6b2abaf703ddeece508c6b9f1549
Zhuan Akbar <zhuan.akb@gmail.com>
2021-06-13 00:16:49 +0700

    Change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://macaroon.xyz/toko-macaron/';
+$config['base_url'] = 'http://toko-macaron.macaroons.xyz/';
 
 /*
 |--------------------------------------------------------------------------
